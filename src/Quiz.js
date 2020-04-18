f316f6d367074c45036d23af945c521205a734e1
indierevo <indierevo@live.com>
2020-04-18 20:42:51 +0100

    completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,7 +8,7 @@ class Quiz extends Component{
     render(){
         return(
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}  />
+                <QuizQuestion />
             </div>
         );
     }
