5d5291986c3b303edbb37b3a12793f5daba5b820
indierevo <indierevo@live.com>
2020-04-19 22:16:56 +0100

    completed module

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {button_text: this.props.quiz_question.answer_options[0]};
-    }
     render(){
         return(
-            <li><button>this.props.button_text</button></li>
+            <li><button>{this.props.button_text}</button></li>
         );
     }
 }
